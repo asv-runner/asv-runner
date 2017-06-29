7f25ca2da97461769da1ccf6adfda70edfea2fcb
Tom Augspurger <tom.w.augspurger@gmail.com>
2017-06-29 13:20:35 -0500

    Log output

--- templates/benchmark_runner.sh
+++ templates/benchmark_runner.sh
@@ -3,8 +3,10 @@
 for f in /home/{{ ansible_user }}/runners/*.sh
 do
     echo "Running ${f}"
+    echo $(date -u +"%Y-%m-%dT%H:%M:%SZ"),start,${f%.*} >> /home/{{ ansible_user }}/runners/log.csv
     ${f}
-    echo "Finished ${f}"
+    echo $(date -u +"%Y-%m-%dT%H:%M:%SZ"),done,${f%.*} >> /home/{{ ansible_user }}/runners/log.csv
 done
 
+echo $(date -u +"%Y-%m-%dT%H:%M:%SZ"),publish,, >> /homme/{{ ansible_user }}/runners/log.csv
 /usr/local/bin/publish
